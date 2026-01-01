3ac1fc53a720508d64daba0686cb9eb0bc4036fd
Omer Ozdemir <oomerozdemir40@gmail.com>
2026-01-01 21:07:50 +0300

    stock parse satiri fix

--- src/middleware/uploadMiddleware.js
+++ src/middleware/uploadMiddleware.js
@@ -1,13 +1,20 @@
 import multer from 'multer';
 import path from 'path';
+import fs from 'fs';
 
 // Dosyanın geçici olarak nereye ve hangi isimle kaydedileceğini belirle
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads/'); // Şimdilik proje ana dizininde 'uploads' klasörü oluşturmalısın
+    const uploadPath = 'uploads/'; 
+
+    if (!fs.existsSync(uploadPath)) {
+      fs.mkdirSync(uploadPath, { recursive: true });
+    }
+
+    cb(null, uploadPath); 
   },
   filename: function (req, file, cb) {
-    // Benzersiz isim oluştur: dosya-adi-zaman.jpg
+    // Dosya ismini benzersiz yap
     cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
   }
 });
@@ -20,6 +27,7 @@ const fileFilter = (req, file, cb) => {
     cb(new Error('Sadece resim dosyaları yüklenebilir!'), false);
   }
 };
+
 
 const upload = multer({ 
   storage: storage,
