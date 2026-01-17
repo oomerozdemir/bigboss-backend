666a1ca3c2245fa2092b93ca95946f0a237abac0
Omer Ozdemir <oomerozdemir40@gmail.com>
2026-01-17 22:44:41 +0300

    paytr structure

--- src/routes/paytrRoutes.js
+++ src/routes/paytrRoutes.js
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 const router = Router();
 import { createPaymentToken, paytrCallback, checkPaymentStatus, testPayment } from '../controller/paytrController.js';
-import { protect} from '../middleware/authMiddleware.js';
+import { protect } from '../middleware/authMiddleware.js';
+
+
 router.post('/create-payment', protect, createPaymentToken);
 
 router.post('/callback', paytrCallback);
